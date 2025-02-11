5693852191ab628e336ececfdb6c56fdd836972e
DonatasNoreika <donoras@gmail.com>
2025-02-11 08:44:08 +0200

    1: Admin puslapyje sukurti meniu punktus visiems sukurtiems modeliams

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import (Service,
+                     CarModel,
+                     Car,
+                     Order,
+                     OrderLine)
 
 # Register your models here.
+admin.site.register(Service)
+admin.site.register(CarModel)
+admin.site.register(Car)
+admin.site.register(Order)
+admin.site.register(OrderLine)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -106,10 +106,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-# TIME_ZONE = 'UTC'
-
 TIME_ZONE = 'Europe/Vilnius'
-USE_TZ = True
+USE_TZ = False
+
+# TIME_ZONE = 'UTC'
+# USE_TZ = True
 
 USE_I18N = True
 
