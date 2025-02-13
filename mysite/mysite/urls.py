45f93ead4c9667f3ba1c11734a8a4e3091a60129
DonatasNoreika <donoras@gmail.com>
2025-02-13 08:04:31 +0200

    3: +Padaryti nukreipimą iš puslapio "/" į "/autoservice" (redirect)

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,6 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('library/', include('library.urls')),
+    path('autoservice/', include('autoservice.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
