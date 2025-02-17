ec8a84d05538dd38afc090cf71f3374e8f7d3552
DonatasNoreika <donoras@gmail.com>
2025-02-17 08:08:50 +0200

    4: +Sukurti puslapį (per funkciją views faile), pvz. autoservice/automobiliai, kuriame būtų atvaizduoti visi servise užregistruoti automobiliai.

--- mysite/autoservice/urls.py
+++ mysite/autoservice/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, cars
 
 urlpatterns = [
     path("", index, name="index"),
+    path("cars/", cars, name="cars"),
 ]
