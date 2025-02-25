a0b383b22713c759e1867f28cbf5cca7fa0b91a6
DonatasNoreika <donoras@gmail.com>
2025-02-25 08:19:15 +0200

    9: +Padaryti, kad profilio puslapyje būtų galima redaguoti vartotojo informaciją, pakeisti nuotrauką.

--- mysite/autoservice/forms.py
+++ mysite/autoservice/forms.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
+from .models import Profile
+
 from .models import OrderComment
 from django import forms
 
@@ -5,3 +8,16 @@ class OrderCommentForm(forms.ModelForm):
     class Meta:
         model = OrderComment
         fields = ['content']
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['photo']
