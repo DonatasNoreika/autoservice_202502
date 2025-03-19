305a59f18ff2cce0f1331fbb277528e38de034bf
DonatasNoreika <donoras@gmail.com>
2025-03-19 10:01:18 +0200

    atnaujintas settings.py

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from .my_settings import (SECRET_KEY,
+                         EMAIL_HOST_USER,
+                         EMAIL_HOST_PASSWORD,
+                         DEBUG,
+                         ALLOWED_HOSTS,
+                         STATICFILES_DIRS,
+                         STATIC_ROOT)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +28,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*f17@3d0&q#(p#0fmk)x#e0w=u+$x#4hnx9b50r6!3&8f2%z=6'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ALLOWED_HOSTS
 
 
 # Application definition
@@ -141,9 +148,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'mano_pastas@gmail.com'
+EMAIL_HOST_USER = EMAIL_HOST_USER
 # el. pašto adresas iš kurio siųsite
-EMAIL_HOST_PASSWORD = 'VerySecret'
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 # slaptažodis
 
 TINYMCE_DEFAULT_CONFIG = {
@@ -176,3 +183,6 @@ TINYMCE_DEFAULT_CONFIG = {
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+STATICFILES_DIRS = STATICFILES_DIRS
+STATIC_ROOT = STATIC_ROOT
